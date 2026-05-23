7e01478d295dde0e959f102e18371bd371e88026
Rahad Ullah <rahadullah10@gmail.com>
2026-05-23 18:20:46 +0600

    fix: reload issue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["10.10.7.79", "via.placeholder.com"],
+  allowedDevOrigins: ["10.10.7.7", "10.10.7.79", "via.placeholder.com"],
   images: {
     remotePatterns: [
       {
