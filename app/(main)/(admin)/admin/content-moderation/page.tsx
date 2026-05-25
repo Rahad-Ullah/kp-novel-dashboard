b64c0a30176e6a4dbc0e5512446a51c8f9ec9c14
Rahad Ullah <rahadullah10@gmail.com>
2026-05-25 12:54:48 +0600

    admin content moderation

--- app/(main)/(admin)/admin/content-moderation/page.tsx
+++ app/(main)/(admin)/admin/content-moderation/page.tsx
@@ -1,11 +1,21 @@
 import Loading from "@/components/common/loading/Loading";
 import ContentModerationLayout from "@/src/admin/content-moderation/ContentModerationLayout";
+import { nextFetch } from "@/utils/nextFetch";
 import { Suspense } from "react";
 
-function ContentModerationPage() {
+async function ContentModerationPage({ searchParams }: { searchParams: any }) {
+  const { searchTerm, type = "book", page, limit } = await searchParams;
+  const query = new URLSearchParams();
+  if (searchTerm) query.set("searchTerm", searchTerm);
+  if (type) query.set("type", type);
+  if (page) query.set("page", page);
+  if (limit) query.set("limit", limit);
+
+  const res = await nextFetch(`/book/book/chapter?${query.toString()}`);
+
   return (
     <Suspense fallback={<Loading />}>
-      <ContentModerationLayout data={{}} meta={{}} />
+      <ContentModerationLayout data={res?.data} meta={res?.meta} />
     </Suspense>
   );
 }
