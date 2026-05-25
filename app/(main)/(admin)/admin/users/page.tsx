2480d010e086c6d0e93edf4897b3baddbb019230
Rahad Ullah <rahadullah10@gmail.com>
2026-05-25 12:49:44 +0600

    users table

--- app/(main)/(admin)/admin/users/page.tsx
+++ app/(main)/(admin)/admin/users/page.tsx
@@ -1,8 +1,17 @@
-import React from "react";
 import UsersLayout from "@/src/admin/users/UsersLayout";
+import { nextFetch } from "@/utils/nextFetch";
 
-function UsersPage() {
-  return <UsersLayout />;
+async function UsersPage({ searchParams }: { searchParams: any }) {
+  const { searchTerm, isActive, page, limit } = await searchParams;
+  const query = new URLSearchParams();
+  if (searchTerm) query.set("searchTerm", searchTerm);
+  if (isActive) query.set("isActive", isActive);
+  if (page) query.set("page", page);
+  if (limit) query.set("limit", limit);
+
+  const res = await nextFetch(`/users/all-users?${query.toString()}`);
+
+  return <UsersLayout users={res?.data} meta={res?.meta} />;
 }
 
 export default UsersPage;
